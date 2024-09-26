04b05a52b185420ca1e47c755887ec6f1132f0cb
Miguel <miguel.napolitano@live.com>
2024-09-25 23:39:43 -0300

    chore: install MaterialUI lib

--- schedule/app/layout.js
+++ schedule/app/layout.js
@@ -1,9 +1,26 @@
-export default function RootLayout({ children }) {
-  return (
-    <html>
-      <body>
-        {children}
-      </body>
-    </html>
-  );
-}
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
+ export default function RootLayout(props) {
+   const { children } = props;
+   return (
+     <html lang="en">
+      <body className={roboto.variable}>
+          <AppRouterCacheProvider>
+           <ThemeProvider theme={theme}>
+              {children}
+           </ThemeProvider>
+          </AppRouterCacheProvider>
+       </body>
+     </html>
+   );
+ }
